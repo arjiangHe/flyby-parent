1ca7caa700180eca2b2c2b724a398fc01ebadd5f
hejiang <hejiang@dream-china.com>
2018-03-27 16:22:32 +0800

    新增

---
+++ corpuser/src/main/java/com/flybycloud/corpuser/controller/GithubController.java
@@ -0,0 +1,26 @@
+package com.flybycloud.corpuser.controller;
+
+import com.alibaba.fastjson.JSON;
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+/**
+ * @author hejiang
+ * @date 2018/3/27
+ */
+@RestController
+public class GithubController {
+
+    private Logger logger = LoggerFactory.getLogger(getClass().getName());
+
+    @RequestMapping(value = "/test/github")
+    public void testGitHub(HttpServletRequest request){
+        Map<String, String[]> map = request.getParameterMap();
+        logger.info(JSON.toJSONString(map));
+    }
+}
---
+++ corpuser/src/test/java/com/flybycloud/corpuser/CorpUserApplicationTests.java
@@ -0,0 +1,16 @@
+package com.flybycloud.corpuser;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CorpUserApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
